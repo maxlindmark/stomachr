b9b4167b0a3abb33c0917f2800721022dc6a9952
Max Lindmark <max.lindmark@slu.se>
2026-08-19 16:46:46 +0200

    now that the download can take ecoregion, we update the bundled example data

--- data-raw/build_example_data.R
+++ data-raw/build_example_data.R
@@ -1,36 +1,24 @@
-# Saves North Sea raw CSVs (2020-2024, Greater North Sea coordinates) to
+# Saves North Sea raw CSVs (2020-2024, Greater North Sea ecoregion) to
 # inst/extdata/ so the vignette can run the pipeline on them with eval=TRUE.
 #
 # Run once with: source("data-raw/build_example_data.R")
 
 pkgload::load_all()
-library(dplyr)
 
 tmp <- tempfile()
-download_stomach(path = tmp, year = 2020:2024)
+download_stomach(path = tmp, year = 2020:2024, ecoregion = "Greater North Sea")
 
 fi   <- readr::read_csv(file.path(tmp, "File_information.csv"),    show_col_types = FALSE)
 hi   <- readr::read_csv(file.path(tmp, "HaulInformation.csv"),     show_col_types = FALSE)
 pred <- readr::read_csv(file.path(tmp, "PredatorInformation.csv"), show_col_types = FALSE)
 prey <- readr::read_csv(file.path(tmp, "PreyInformation.csv"),     show_col_types = FALSE)
 
-# Filter hauls to Greater North Sea
-hi_ns <- hi |> filter(
-  !is.na(ShootLat), !is.na(ShootLong),
-  ShootLat >= 51, ShootLat <= 62,
-  ShootLong >= -4, ShootLong <= 13
-)
-
-pred_ns <- pred |> filter(tblHaulID %in% hi_ns$tblHaulID)
-prey_ns <- prey |> filter(tblPredatorInformationID %in% pred_ns$tblPredatorInformationID)
-fi_ns   <- fi   |> filter(tblUploadID %in% hi_ns$tblUploadID)
-
 dir.create("inst/extdata", recursive = TRUE, showWarnings = FALSE)
-readr::write_csv(fi_ns,   "inst/extdata/File_information.csv")
-readr::write_csv(hi_ns,   "inst/extdata/HaulInformation.csv")
-readr::write_csv(pred_ns, "inst/extdata/PredatorInformation.csv")
-readr::write_csv(prey_ns, "inst/extdata/PreyInformation.csv")
+readr::write_csv(fi,   "inst/extdata/File_information.csv")
+readr::write_csv(hi,   "inst/extdata/HaulInformation.csv")
+readr::write_csv(pred, "inst/extdata/PredatorInformation.csv")
+readr::write_csv(prey, "inst/extdata/PreyInformation.csv")
 
 cli::cli_inform(c(
-  "v" = "inst/extdata/ written ({nrow(pred_ns)} predators, Greater North Sea 2020-2024)"
+  "v" = "inst/extdata/ written ({nrow(pred)} predators, Greater North Sea ecoregion, 2020-2024)"
 ))
